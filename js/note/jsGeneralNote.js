1a6d4ff3065de32d0d8e1f418d7411c65744eab2
Long Nguyen <ndtlong0611@gmail.com>
2021-10-19 12:57:36 -0500

    add general notes on how to interact with browser and HTML from JS file

---
+++ js/note/jsGeneralNote.js
@@ -0,0 +1,15 @@
+// - console.log() == print() in  C++
+
+// console.warn('warning_message')
+    // - give warning message in console 
+// console.error("error_message")
+    // - give error message in console 
+// alert() 
+    // - no console. needed
+    // show a pop-up warning on the current page
+// prompt()
+    // - prompt user to enter
+
+
+// parseInt(str)
+    // - parse str into a number
