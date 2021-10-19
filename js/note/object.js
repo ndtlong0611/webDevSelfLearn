3e2a193576c3ff7633e36da4999d7292051058e4
Long Nguyen <ndtlong0611@gmail.com>
2021-10-19 11:21:01 -0500

    First commit for JS Notes

---
+++ js/note/object.js
@@ -0,0 +1,17 @@
+// Object is created with: 
+const testObject = {
+    attrbt1: "true",
+    attrbt2: "blue",
+    attrbt3: "metal",
+};
+
+// Object information can be accessed with:
+// 1. Dot notation: 
+testObject.attrbt1 
+// 2. Square Bracket:
+testObject['attrbt2']
+
+// New attributes can be added as follow:
+testObject.attrbt4 = "wing"
+// Or
+testObject["attrbt5"] = "spoiler"
---
+++ js/note/primitiveTyes.js
@@ -0,0 +1,22 @@
+// Use Chrome console to test out JS code
+// REPL Console - Read Evaluate Print Loop
+
+// 1. Numbers: 
+// - JS has one number type: Positive, Negative, Whole (int) and Decimal numbers. 
+// - Math Ops: Add, Sub, Multiply, Div, Modulo (%), Exponent (**)
+// Follow PEMDAS, Left -> Right
+
+// NaN means "Not a Number"
+// 0 / 0 = NaN
+// 1 + NaN = NaN
+
+// 2. Variables: 
+// syntax: let varName = value; 
+// Or const: let varName = value; <---- But you can't change the value later.
+// Or an ancient way (but still works like "let":): var varName = value;
+
+// Updating variables:
+// a = a + 5;
+// a += 5;
+// Above 2 are the same, applies for -, *, /
+// Or ++, --
