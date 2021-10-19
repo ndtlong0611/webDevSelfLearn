4b1a8afa81f0f7d5735d7848da9e75d14c108a94
Long Nguyen <ndtlong0611@gmail.com>
2021-10-19 15:14:25 -0500

    finish array.js note

---
+++ js/note/array.js
@@ -0,0 +1,22 @@
+arr = [1,2,3,4,5]
+// here is an example of array
+arr[2] 
+// accessing an array element is like
+
+
+// Array in JS can contain all types of data
+mixedBag = [2, 'as', 828, '231', null, undefined]
+
+// Array's elements can be changed (values and type-wise)
+mixedBag[3] = "2868"
+
+// Array's methods:
+// - Push: basically add a new element at the end of the array 
+mixedBag.push("hello")
+// - Pop: remove element at the end of the array
+mixedBag.pop()
+
+// - Shift: Remove to the beginning of the array
+ mixedBag.shift()
+// - Unshift: Add to the beginning of the array
+mixedBag.unshift("unshiftMeansAdd")
