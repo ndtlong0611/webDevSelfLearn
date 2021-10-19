26a04f9a26084af3b7480447cf6764d2267837a2
Long Nguyen <ndtlong0611@gmail.com>
2021-10-19 15:15:00 -0500

    final version of note about JS conditional

--- js/note/decisionMaking.js
+++ js/note/decisionMaking.js
@@ -9,3 +9,15 @@
 // FALSE: 0 === false
 
 // SHOULD USE TRIPLE EQUAL
+
+// To check for space in a string, use str.indexOf(' '):
+// - it will return -1 if none space was found.
+
+// True And False: 
+// - Everything in JS considered true, except for: 
+//     + false
+//     + 0
+//     + "" (empty string)
+//     + null
+//     + undefined
+//     + NaN
