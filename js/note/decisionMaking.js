18fa28a54121faed7c8b4faddf56018090bd58ec
Long Nguyen <ndtlong0611@gmail.com>
2021-10-19 12:56:47 -0500

    add decisionMaking/conditional note file

---
+++ js/note/decisionMaking.js
@@ -0,0 +1,11 @@
+// - Comparison Operators:
+//     >, <, =>, =<, 
+//     ==, !=, 
+//     ===, !== (strict equality & strict non-equality) ,-- check for type
+// TRUE: 1 == '1'    TRUE: 1 !== '1'
+// FALSE: 1 === '1' 
+
+// TRUE: 0 == false;
+// FALSE: 0 === false
+
+// SHOULD USE TRIPLE EQUAL
