3273139fbb18a7b8ae283a2a7de821ea23abef86
Long Nguyen <ndtlong0611@gmail.com>
2021-10-20 13:51:50 -0500

    add some test functions

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("FIRST JS FILE EVER!")
+
+function rant(rantStr) {
+    console.log(rantStr.toUpperCase())
+    console.log(rantStr.toUpperCase())
+    console.log(rantStr.toUpperCase())
+}
+
+function sumArray(array) {
+    let sum = 0;
+    for(i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+    return sum;
+}
+
+dateArr = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday"
+}
