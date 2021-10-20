775db2e0a46016735fb3578e5910fbc8d7537426
Long Nguyen <ndtlong0611@gmail.com>
2021-10-20 12:11:49 -0500

    first version of loop notes

---
+++ js/note/loop.js
@@ -0,0 +1,12 @@
+// Loop: 
+// - for loop
+// - while loop
+// - for ... of loop
+// - for ... in loop
+
+// For Loop Syntax:
+// for (
+//     [initialExpression];
+//     [condition];   <-------------- semicolon
+//     [incrementExpression] (or decrement)
+// )
